bd4959dc03fec0e2d2c48a8457d96eb9fb6d26a6
Chirayu <chirayu47sharma@gmail.com>
2025-03-01 21:37:26 +0530

    Strings

---
+++ Strings/String-subsequence.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+// in subsequence we if a string have "n" alphabets then there will be "2^n" possiblity of string subsequence 
+// the time complexity of this code will be O(n+m)
+bool subseq(string stri, string strj){
+    int i=0;
+    int j=0;
+    while (j<strj.length()&&i<stri.length()){
+        if(stri[i]==strj[j]) {
+            j++;
+            }
+        i++;
+        }
+        return (j==strj.length());// when all alpha completed t
+    }
+
+int main(){
+    string stri;
+    cout << "Enter a sring: ";
+    cin >> stri;
+    string strj;
+    cout << "Enter a subsequent string if it is a true it will print yes else no ";
+    cin >> strj;
+    cout << "Is the string a Subsequent? " << (subseq(stri,strj) ? "YES":"NO") << endl;
+    return 0;
+}
---
+++ Strings/string-anagram.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<algorithm>
+// To check is a string is a permutation or not of each other--> also called anagram
+using namespace std;
+
+bool ana(const string &str1, const string &str2){
+    if(str1.length()!=str2.length()){
+        return false;
+    }
+    sort(str1.begin(),str1.end());
+    sort(str2.begin(),str2.end());
+    return str1==str2;
+}
+
+int main(){
+    string str1, str2; 
+    cout<<"enter a string: "<<endl; cin>>str1;
+    cout<<"enter a string to find that a if it is a anagram or not: "<<endl; cin>>str2;
+    cout<<"Is string an anagram: "<< (ana(str1,str2)? "Yes":"NO")<<endl;
+    return 0;
+}
